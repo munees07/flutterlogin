bca8f7d80bfdca562609c0f1643b4dc76126c6c9
munees07 <muneezshanu5661@gmail.com>
2023-12-14 15:17:00 +0530

    alt

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,18 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
 import 'login.dart';
 
 void main() {
   runApp(const NewApp());
 }
 
-class NewApp extends StatefulWidget {
+class NewApp extends StatelessWidget {
   const NewApp({super.key});
 
   @override
-  State<NewApp> createState() => _NewAppState();
-}
-
-class _NewAppState extends State<NewApp> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Login(),
-      routes: {Home.routeName: (context) => const Home()},
+      home: Login(),
     );
   }
 }
